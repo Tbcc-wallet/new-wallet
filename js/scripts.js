627737718d39eab76c17b8d9c575a57f4b80d512
Екатерина Лукинова <katyushta@rambler.ru>
2020-09-08 08:29:45 +0300

    ch

--- js/scripts.js
+++ js/scripts.js
@@ -18,44 +18,44 @@ $( document ).ready(function() {
     }
 
 
-
-    var result = [];
-
-    $.get( "https://dex.binance.org/api/v1/mini/ticker/24hr?symbol=VTBC-C26M_BNB", function( outerData ) {
-        $.get( "https://dex.binance.org/api/v1/mini/ticker/24hr", function( data ) {
-            $.get( "https://dex.binance.org/api/v1/mini/tokens", function( innerData ) {
-                $.get( "https://dex.binance.org/api/v1/ticker/24hr", function( innerInnerData ) {
-                    // console.log(data)
-                    // console.log(innerData)
-                    // console.log(innerInnerData)
-                    var bnbToUsd = parseFloat(innerInnerData.find(x => x.symbol == 'BNB_BUSD-BD1').lastPrice);
-                    var btbcToBnb = parseFloat(data.find(x => x.symbol == 'VTBC-C26M_BNB').lastPrice);
-                    for (let i = 0; i < data.length; i++) {
-                        var token = innerData.find(x => data[i].baseAssetName == x.symbol)
-                        var totalSupply = null;
-                        if (token) {
-                            totalSupply = token.total_supply;
-                        }
-                        result.push({
-                            capitalization: parseFloat(totalSupply) * parseFloat(data[i].lastPrice),
-                            symbol: data[i].symbol
-                        });
-                    }
-
-                    result.sort((a, b) => (a.capitalization > b.capitalization) ? -1 : 1)
-                    result = result.filter(x => x.capitalization > 0);
-                    // console.log(result)
-                    var rank = result.findIndex(x => x.symbol == 'VTBC-C26M_BNB') + 1;
-                    $( "#global-rank" ).html( rank );
-
-                    $( "#price" ).html( '$' + parseFloat(bnbToUsd * btbcToBnb).toFixed(3) );
-                    $( "#volume" ).html( parseFloat(outerData[0].quoteVolume).toFixed(3) + ' ' + 'BNB' );
-                    $( "#total-market-cap" ).html( '$' + parseFloat(bnbToUsd * btbcToBnb * 1000000).toFixed(0) );
-                    $( "#vtbc-key" ).html(parseFloat( 26 / (bnbToUsd * btbcToBnb)).toFixed(2) + ' ' + 'VTBC');
-                });
-            });
-        });
-    });
+    //
+    // var result = [];
+    //
+    // $.get( "https://dex.binance.org/api/v1/mini/ticker/24hr?symbol=VTBC-C26M_BNB", function( outerData ) {
+    //     $.get( "https://dex.binance.org/api/v1/mini/ticker/24hr", function( data ) {
+    //         $.get( "https://dex.binance.org/api/v1/mini/tokens", function( innerData ) {
+    //             $.get( "https://dex.binance.org/api/v1/ticker/24hr", function( innerInnerData ) {
+    //                 // console.log(data)
+    //                 // console.log(innerData)
+    //                 // console.log(innerInnerData)
+    //                 var bnbToUsd = parseFloat(innerInnerData.find(x => x.symbol == 'BNB_BUSD-BD1').lastPrice);
+    //                 var btbcToBnb = parseFloat(data.find(x => x.symbol == 'VTBC-C26M_BNB').lastPrice);
+    //                 for (let i = 0; i < data.length; i++) {
+    //                     var token = innerData.find(x => data[i].baseAssetName == x.symbol)
+    //                     var totalSupply = null;
+    //                     if (token) {
+    //                         totalSupply = token.total_supply;
+    //                     }
+    //                     result.push({
+    //                         capitalization: parseFloat(totalSupply) * parseFloat(data[i].lastPrice),
+    //                         symbol: data[i].symbol
+    //                     });
+    //                 }
+    //
+    //                 result.sort((a, b) => (a.capitalization > b.capitalization) ? -1 : 1)
+    //                 result = result.filter(x => x.capitalization > 0);
+    //                 // console.log(result)
+    //                 var rank = result.findIndex(x => x.symbol == 'VTBC-C26M_BNB') + 1;
+    //                 $( "#global-rank" ).html( rank );
+    //
+    //                 $( "#price" ).html( '$' + parseFloat(bnbToUsd * btbcToBnb).toFixed(3) );
+    //                 $( "#volume" ).html( parseFloat(outerData[0].quoteVolume).toFixed(3) + ' ' + 'BNB' );
+    //                 $( "#total-market-cap" ).html( '$' + parseFloat(bnbToUsd * btbcToBnb * 1000000).toFixed(0) );
+    //                 $( "#vtbc-key" ).html(parseFloat( 26 / (bnbToUsd * btbcToBnb)).toFixed(2) + ' ' + 'VTBC');
+    //             });
+    //         });
+    //     });
+    // });
 
     var dictionary = {
         EN: {
